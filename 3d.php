14b77de81cfe25ba53db1b9a06105592c6366bb9
magnus <magnus.ja.gustafsson@gmail.com>
2017-02-27 23:29:37 +0100

    Created img/2d folder

--- 3d.php
+++ 3d.php
@@ -8,7 +8,7 @@
  <html>
    <head>
      <meta charset="utf-8">
-     <title>this is 3d!</title>
+     <title>Magnus Gustafsson - 3D</title>
      <link rel="stylesheet" href="css/nav.css">
      <link rel="stylesheet" href="css/master.css">
      <link rel="stylesheet" href="css/3d.css">
@@ -27,7 +27,23 @@
         foreach ($json['img3D'] as $key => $value) {
           echo '<div class="container">';
             echo '<div class="imgContainer">';
-              echo '<img src='.$imgPath.$value['src'].'>';
+              //echo '<iframe src='.$value['src'][0].' class="video">'
+
+              /*<div style="position:relative;height:0;padding-bottom:56.25%">
+                <iframe src="https://www.youtube.com/embed/rVbsUTJoodU?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen>
+                </iframe>
+              </div>*/
+            // foreach ($value['src'] as $img) {
+            //   if ($value["type"] == "vid") {
+            //     echo '<iframe src="https://www.youtube.com/embed/rVbsUTJoodU?ecver=2" width="100%">';
+            //   } elseif ($value["type"] == "img") {
+            //     echo '<img src='.$imgPath.$img.'>';
+            //   }
+            // }
+            foreach ($value["src"] as $img) {
+              echo '<img src='.$imgPath.$img.'>';
+            }
+
             echo '</div>';
             echo '<div class="infoContainer">';
               echo '<h2>'.$value['title'].'</h2>';
